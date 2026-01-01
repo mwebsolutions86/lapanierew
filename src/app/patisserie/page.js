ae804a885e6031b98aed0ba98133ca4e5f78763c
mwebsolutions86 <mazouz86mohcine@hotmail.com>
2026-01-01 01:35:40 +0100

    update

--- src/app/patisserie/page.js
+++
@@ -1,5 +0,0 @@
-import ImmersiveCollection from '@/components/templates/ImmersiveCollection';
-
-export default function Page() {
-  return <ImmersiveCollection title="Pâtisserie" category="Pâtisserie" description="L'élégance sucrée, des classiques revisités avec finesse." />;
-}
